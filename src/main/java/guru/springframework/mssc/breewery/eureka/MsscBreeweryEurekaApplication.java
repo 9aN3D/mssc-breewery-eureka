0f3b7e98362f31837ca24cb60b3a8644e6e959ce
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-06-30 14:24:52 +0200

    init

---
+++ src/main/java/guru/springframework/mssc/breewery/eureka/MsscBreeweryEurekaApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.breewery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreeweryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreeweryEurekaApplication.class, args);
+	}
+
+}
