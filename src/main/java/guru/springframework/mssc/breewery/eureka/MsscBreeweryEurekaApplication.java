12b1d44843b341e1afef1b66636d9e72725f0152
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-06-30 14:42:44 +0200

    Init configurations

--- src/main/java/guru/springframework/mssc/breewery/eureka/MsscBreeweryEurekaApplication.java
+++ src/main/java/guru/springframework/mssc/breewery/eureka/MsscBreeweryEurekaApplication.java
@@ -2,11 +2,14 @@ package guru.springframework.mssc.breewery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MsscBreeweryEurekaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MsscBreeweryEurekaApplication.class, args);
 	}
 
